a49ba5ddb432ca7c38b02d8710c078a72743c0f5
ImStyrene <ImStyrene@protonmail.com>
2025-09-01 10:39:17 -0400

    Update init.lua

--- packer-gui/lua/packer-gui/init.lua
+++ packer-gui/lua/packer-gui/init.lua
@@ -1,7 +1,10 @@
 local M = {}
 
-function M.open()
-  require("packer-gui.ui").open()
-end
+M.open = require("packer-gui.ui").open
+
+-- Create :Packer command
+vim.api.nvim_create_user_command("Packer", function()
+  M.open()
+end, {})
 
 return M
