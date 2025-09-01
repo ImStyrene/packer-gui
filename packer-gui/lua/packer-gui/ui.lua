c112886d3bee8b1dd08bf13ff0383a0b0afa1e6f
ImStyrene <ImStyrene@protonmail.com>
2025-09-01 11:20:07 -0400

    Update ui.lua

--- packer-gui/lua/packer-gui/ui.lua
+++ packer-gui/lua/packer-gui/ui.lua
@@ -1,11 +1,14 @@
 local M = {}
 
 function M.open()
+  -- Open a new scratch buffer in a vertical split
   vim.cmd("vsplit")
   vim.cmd("enew")
   vim.bo.buftype = "nofile"
   vim.bo.bufhidden = "wipe"
   vim.bo.swapfile = false
+
+  -- Set ASCII menu
   vim.api.nvim_buf_set_lines(0, 0, -1, false, {
     "########################################",
     "#           Packer GUI Menu             #",
@@ -15,15 +18,16 @@ function M.open()
     "[I] Install",
     "[C] Compile",
     "[U] Update",
-    "[Q] Quit",
+    "[q] Quit",
   })
 
+  -- Buffer-local keymaps
   local opts = { noremap = true, silent = true, buffer = 0 }
   vim.keymap.set("n", "S", "<cmd>PackerSync<CR>", opts)
   vim.keymap.set("n", "I", "<cmd>PackerInstall<CR>", opts)
   vim.keymap.set("n", "C", "<cmd>PackerCompile<CR>", opts)
   vim.keymap.set("n", "U", "<cmd>PackerUpdate<CR>", opts)
-  vim.keymap.set("n", "Q", "<cmd>bd!<CR>", opts)
+  vim.keymap.set("n", "q", "<cmd>bd!<CR>", opts)
 end
 
 return M
